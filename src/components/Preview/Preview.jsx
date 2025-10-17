982309cba1890a53bcf2b93bb1718533c8a441b0
SoleneSol <so.buono@gmail.com>
2025-10-17 11:51:43 +0200

    test avec un audio

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,12 +1,10 @@
 import "./Preview.scss";
 import { useState } from "react";
+import { useEffect } from "react";
 
-function Preview({title, genres}) {
+function Preview({title, audiosrc, genres}) {
 
-    // const audio = document.getElementById("audio");
-    const play = document.querySelector('.button__play');
-    const pause = document.querySelector('.button__pause');
-
+    const [audioElement, setAudioElement] = useState();
     const [playIsHidden, setPlayIsHidden] = useState(false);
     const [pauseIsHidden, setPauseIsHidden] = useState(true);
 
@@ -16,14 +14,18 @@ function Preview({title, genres}) {
     }
 
     function handlePlay() {
-        // audio.play();
+        audioElement.play();
         toggleHidden();
     }
 
     function handlePause() {
-        // audio.pause();
+        audioElement.pause();
         toggleHidden();
     }
+
+    useEffect(() => {
+        setAudioElement(new Audio(audiosrc));
+    }, [audiosrc])
 
     return (
         <>
@@ -31,9 +33,9 @@ function Preview({title, genres}) {
             <div className="preview__container">
                 <figure className="preview__title__container">
                     <figcaption className="preview__title">{title}</figcaption>
-                    <audio id="audio">
+                    <audio className="audio">
                         {/* à mettre la source dynamiquement, et le type */}
-                        <source src="null"/>
+                        <source src={audiosrc} type="audio/mpeg"/>
                     </audio>
                     <div className="buttons">
                         <div className={playIsHidden ? "button button__play hidden" : "button button__play"} onClick={handlePlay} id="play">
--- src/components/PreviewList/PreviewList.jsx
+++ src/components/PreviewList/PreviewList.jsx
@@ -5,15 +5,16 @@ function PreviewList() {
 
     // test genres en dur pour map ensuite
     const genres = ["pop", "rock", "classique"]
+    const audioscr = "/src/assets/RAYE.mp3"
 
     return (
         <>
                 <h1>Tous les extraits</h1>
                 <section className="previewList">
                     {/* Pour le moment en dur pour les tests */}
-                    <Preview title="titre 1" genres={genres}/>
-                    <Preview title="titre 2" genres={genres}/>
-                    <Preview title="titre 3" genres={genres}/>
+                    <Preview audiosrc={audioscr} title="titre 1" genres={genres}/>
+                    <Preview audiosrc={audioscr} title="titre 2" genres={genres}/>
+                    <Preview audiosrc={audioscr} title="titre 3" genres={genres}/>
                 </section>
         </>
     )
