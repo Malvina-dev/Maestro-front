64a86d03e14bba57e7d95d6b030452f699d78ba0
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-10-22 16:56:21 +0200

    footer dynamique wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Header from './components/Header/Header.jsx'
+/* import Header from './components/Header/Header.jsx' */
 import Footer from './components/Footer/Footer.jsx';
 import './index.css';
 function App() {
 
   return (
     <div className='App'>
-        <Header />
+        {/* <Header /> */}
         <main>
           
         </main>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import './Footer.scss';
 
+const links = [
+    { label: "Nous contacter", href: "#" },
+    { label: "Politique de confidentialité", href: "#" },
+    { label: "Informations légales", href: "#" },
+    { label: "CGU", href: "#" },
+    { label: "Accessibilité", href: "#" },
+];
+
 function Footer() {
     return (
         <footer>
             <nav>
                 <ul>
-                    <li><a href="#">Nous contacter</a></li>
-                    <li><a href="#">Politique de confidentialité</a></li>
-                    <li><a href="#">Informations légales</a></li>
-                    <li><a href="#">CGU</a></li>
-                    <li><a href="#">Accessibilité</a></li>
+                    {links.map((link, index) => (
+                        <li key={index}>
+                            <a href={link.href}>{link.label}</a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </footer>
-    )
+    );
 }
 
 export default Footer;
