be35dfa9661f11e7208c05007ff4bc334de87f54
Malvina <malvina.gilardeau@gmail.com>
2025-10-27 14:26:59 +0100

    feat: add ProjectForm component with API POST integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
-import Header from './components/Header/Header.jsx'
-import Footer from './components/Footer/Footer.jsx';
-import Description from './components/Description/Description'
-import PreviewList from './components/PreviewList/PreviewList.jsx'
-import ProjectForm from './components/ProjectForm/ProjectForm.jsx';
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Description from "./components/Description/Description";
+import PreviewList from "./components/PreviewList/PreviewList.jsx";
+import ProjectForm from "./components/ProjectForm/ProjectForm.jsx";
 import ClientCard from "./components/ClientList/Clientcard/ClientCard.jsx";
 import ClientInline from "./components/ClientList/ClientInline/ClientInline.jsx";
-import ClientList from "./components/ClientList/ClientList.jsx"; 
+import ClientList from "./components/ClientList/ClientList.jsx";
 
 import "./index.css";
 
+
 function App() {
-
-  return (
-    <div className='App'>
-
-
-        
-        <Header />
-        <main>
-      <Description />
-    <ProjectForm />
-          <PreviewList />
-          <ClientCard />
-          <ClientInline />
-          <ClientList />  
-        </main>
-        <Footer />
-          
-          
-    </div>
-  );
-
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Description />
+                <ProjectForm />
+                <PreviewList />
+                {/* <ClientList /> */}
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,24 +1,54 @@
 import "./ProjectForm.scss";
-
-
+import { createProject } from "../../api/apiProject.js";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function ProjectForm() {
+    // useState permet de stocker et mettre à jour les valeurs du formulaire.
+    const [name, setName] = useState("");
+    const [resume, setResume] = useState("");
+
+    // Fonction appelée quand l'utilisateur valide le formulaire
+    const handleCreateProject = (e) => {
+        e.preventDefault(); // empêche la page de se recharger
+
+        // crée un objet avec les données saisies par l'utilisateur 
+        const projectData = { name: name, resume: resume };
+        // envoie les données
+        createProject(projectData);
+    };
+
     return (
-        <Form className="project-frm">
+        <Form className="project-frm" onSubmit={(e) => handleCreateProject(e)}> // onSubmit = exécute la fonction "handleCreateProject" quand le formulaire est soumis
             <h2 className="title">Nouvelle demande de projet</h2>
 
             {/* Titre du projet */}
             <Form.Group className="form__title" controlId="FormTitleProject">
-                <Form.Label className="form__title">Titre du projet*</Form.Label>
-                <Form.Control className="form__titleInput" type="text"/>
+                <Form.Label className="form__title">
+                    Titre du projet*
+                </Form.Label>
+                <Form.Control
+                    className="form__titleInput"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
             </Form.Group>
 
             {/* Description */}
-            <Form.Group className="form__Description" controlId="FormDescription">
+            <Form.Group
+                className="form__Description"
+                controlId="FormDescription"
+            >
                 <Form.Label>Description du projet*</Form.Label>
-                <Form.Control className="form__descriptionInput"  as="textarea" rows={4}/>
+                <Form.Control
+                    className="form__descriptionInput"
+                    as="textarea"
+                    rows={4}
+                    value={resume}
+                    onChange={(e) => setResume(e.target.value)}
+                />
             </Form.Group>
 
             {/* bouton*/}
@@ -27,7 +57,6 @@ function ProjectForm() {
             </Button>
             <p className="form__champsObligatoire">*champs obligatoires</p>
         </Form>
-        
     );
 }
 
