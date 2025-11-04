e1c480e5e38f2e15232868bc0c54dc4832d6854e
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-11-04 16:09:45 +0100

    icone show password ok

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,22 +1,22 @@
 import "./LoginForm.scss";
 import { loginUser } from "../../api/apiUser.js";
 import { useState, useContext } from "react";
-import { Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import { Button, Form, InputGroup } from "react-bootstrap";
 import UserContext from "../../UserContext.jsx";
+import { Eye, EyeSlash } from "react-bootstrap-icons";
 
-function LoginForm({setUserHasAccount}) {
+function LoginForm({ setUserHasAccount }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
     // il va falloir appeler le userContext, res.data.user(.role)
-    const {loginProvider} = useContext(UserContext);
+    const { loginProvider } = useContext(UserContext);
 
     async function handelSubmit(event) {
         event.preventDefault();
         const loginData = { email: email, password: password };
         const userInfo = await loginUser(loginData);
-        console.log('userInfo ',userInfo);
-        
+        console.log("userInfo ", userInfo);
         loginProvider(userInfo.user.role);
     }
 
@@ -53,14 +53,24 @@ function LoginForm({setUserHasAccount}) {
                     controlId="password"
                 >
                     <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control
-                        className="login-form-password-input"
-                        type="password"
-                        placeholder="Entrez votre mot de passe"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
+                    <InputGroup>
+                        <Form.Control
+                            className="login-form-password-input"
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Entrez votre mot de passe"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+                        <Button
+                            variant="outline-secondary"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="login-form-eye-button"
+                        >
+                            {showPassword ? <EyeSlash /> : <Eye />}
+                        </Button>
+                    </InputGroup>
                 </Form.Group>
+
                 <Button
                     className="login-form-button"
                     variant="login-form-button"
@@ -69,7 +79,11 @@ function LoginForm({setUserHasAccount}) {
                     Se connecter
                 </Button>
             </Form>
-                <p>Pas encore de compte ? Veuillez vous <Button onClick={handleRegister}>Inscrire</Button></p>
+
+            <p>
+                Pas encore de compte ? Veuillez vous{" "}
+                <Button onClick={handleRegister}>Inscrire</Button>
+            </p>
         </>
     );
 }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,117 +1,112 @@
 import React, { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Eye, EyeSlash } from "react-bootstrap-icons";
 import "./RegisterForm.scss";
 import { create } from "../../api/apiUser";
 
-function RegisterForm({setUserHasAccount}) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setconfirmPassword] = useState("");
+function RegisterForm({ setUserHasAccount }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    function handelSubmit(event) {
-        event.preventDefault();
-        if (password !== confirmPassword) {
-            alert("Les mots de passe ne correspondent pas");
-            return;
-        }
-        const registerData = {
-            email: email,
-            password: password,
-            confirmPassword: confirmPassword,
-        };
-        create(registerData)
-            .then((response) => {
-                console.log("inscription réussi", response);
-            })
-            .catch((error) => {
-                console.error("inscription échoué", error);
-            });
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas");
+      return;
     }
+    create({ email, password, confirmPassword })
+      .then((response) => console.log("Inscription réussie", response))
+      .catch((error) => console.error("Inscription échouée", error));
+  }
 
-    function handleLogin(event) {
-        event.preventDefault();
-        setUserHasAccount(true);
-    }
+  function handleLogin(event) {
+    event.preventDefault();
+    setUserHasAccount(true);
+  }
 
-    return (
-        <div className="register-wrapper">
-            <div className="header-register">
-                <h2>Créer un compte</h2>
-                <p>Les champs marqués d'un astérisque (*) sont obligatoires</p>
-            </div>
+  return (
+    <div className="register-wrapper">
+      <div className="header-register">
+        <h2>Créer un compte</h2>
+        <p>Les champs marqués d'un astérisque (*) sont obligatoires</p>
+      </div>
 
-            <Container className="register-container">
-                <Row className="justify-content-md-center">
-                    <Col md={6}>
-                        <Form
-                            method="post"
-                            onSubmit={(event) => handelSubmit(event)}
-                        >
-                            <Form.Group controlId="email" className="mb-3">
-                                <Form.Label>Adresse-mail *</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Entrez votre adresse-mail"
-                                    name="email"
-                                    value={email}
-                                    onChange={(event) =>
-                                        setEmail(event.target.value)
-                                    }
-                                    required
-                                />
-                            </Form.Group>
+      <Container className="register-container">
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <Form method="post" onSubmit={handleSubmit}>
+              <Form.Group controlId="email" className="mb-3">
+                <Form.Label>Adresse-mail *</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Entrez votre adresse-mail"
+                  name="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  required
+                />
+              </Form.Group>
 
-                            <Form.Group controlId="password" className="mb-3">
-                                <Form.Label>Mot de passe *</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Entrez votre mot de passe"
-                                    name="password"
-                                    value={password}
-                                    onChange={(event) =>
-                                        setPassword(event.target.value)
-                                    }
-                                    required
-                                />
-                            </Form.Group>
+              <Form.Group controlId="password" className="mb-3 password-wrapper">
+                <Form.Label>Mot de passe *</Form.Label>
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Entrez votre mot de passe"
+                  name="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                />
+                <Button
+                  variant="link"
+                  type="button"
+                  className="show-password-btn"
+                  onClick={() => setShowPassword(!showPassword)}
+                  tabIndex={-1}
+                >
+                  {showPassword ? <EyeSlash size={20} /> : <Eye size={20} />}
+                </Button>
+              </Form.Group>
 
-                            <Form.Group
-                                controlId="ConfirmPassword"
-                                className="mb-3"
-                            >
-                                <Form.Label>
-                                    Confirmer le mot de passe *
-                                </Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Confirmer votre mot de passe"
-                                    name="confirmPassword"
-                                    value={confirmPassword}
-                                    onChange={(event) =>
-                                        setconfirmPassword(event.target.value)
-                                    }
-                                    required
-                                />
-                            </Form.Group>
-                            <div className="form-actions">
-                                <Button
-                                    type="submit"
-                                    className="rounded-btn"
-                                >
-                                    S'inscrire
-                                </Button>
-                                {/* <p className="mt-3 mb-0">
-                                    Déjà un compte ?
-                                    <a href="#">Se connecter</a>
-                                </p> */}
-                            </div>
-                        </Form>
-                        <p>Déjà un compte ? <Button onClick={handleLogin}>Connexion</Button></p>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+              <Form.Group controlId="confirmPassword" className="mb-3 password-wrapper">
+                <Form.Label>Confirmer le mot de passe *</Form.Label>
+                <Form.Control
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirmer votre mot de passe"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={e => setConfirmPassword(e.target.value)}
+                  required
+                />
+                <Button
+                  variant="link"
+                  type="button"
+                  className="show-password-btn"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  tabIndex={-1}
+                >
+                  {showConfirmPassword ? <EyeSlash size={20} /> : <Eye size={20} />}
+                </Button>
+              </Form.Group>
+
+              <div className="form-actions">
+                <Button type="submit" className="rounded-btn">
+                  S'inscrire
+                </Button>
+            <p >
+              Déjà un compte ? <Button onClick={handleLogin}>Connexion</Button>
+            </p>
+              </div>
+            </Form>
+
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default RegisterForm;
