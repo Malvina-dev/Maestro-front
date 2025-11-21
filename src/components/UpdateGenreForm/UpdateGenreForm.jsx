5265757007cde5acbfd8c6ff694ed02d55dfb3d6
CynthiaManier <cynthia.manier.pro@gmail.com>
2025-11-21 13:23:08 +0100

    Added toast to the UpdateGenreForm  component to update a genre

--- src/components/UpdateGenreForm/UpdateGenreForm.jsx
+++ src/components/UpdateGenreForm/UpdateGenreForm.jsx
@@ -1,20 +1,18 @@
 import { useEffect, useState } from "react";
 import { updateGenre } from "../../api/apiGenre.js";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { notify } from "../Toast/Toast.jsx";
 
-function UpdateGenreForm({genre, onSaved = () => {}, unshow = () => {} }) {
-
+function UpdateGenreForm({ genre, onSaved = () => {}, unshow = () => {} }) {
     const [formLabel, setFormLabel] = useState(genre.label);
     const [saving, setSaving] = useState(false);
     const [error, setError] = useState(null);
 
-
-
     async function handleSubmit(e) {
         e.preventDefault();
         setSaving(true);
-        
+
         try {
             await updateGenre(genre.id, formLabel);
             onSaved();
@@ -24,6 +22,7 @@ function UpdateGenreForm({genre, onSaved = () => {}, unshow = () => {} }) {
         } finally {
             setSaving(false);
             unshow();
+            notify("Genre créé avec succès", "success");
         }
     }
 
@@ -34,22 +33,35 @@ function UpdateGenreForm({genre, onSaved = () => {}, unshow = () => {} }) {
 
     return (
         <>
-        {error && <p className="text-danger">{error}</p>}
-        <Form onSubmit={handleSubmit} id='updateGenre' method='patch'>
-            <h2 className="form__title">Modifier le genre</h2>
-            <Form.Group className="mb-3 form__group">
-                <Form.Label className='form__label' htmlFor='genreLabel'>Genre</Form.Label>
-                <Form.Control className='form__input' value={formLabel} onChange={(e) => setFormLabel(e.target.value)} id='genreLabel' name='label' type="text" placeholder="Entrer le genre" />
-            </Form.Group>
-            <div className="d-flex form__button__container">
-                <Button className="genre__form__button" type="submit" disabled={saving}>
-                    {saving ? "Enregistrement..." : "Enregistrer"}
-                </Button>
-            </div>
-        </Form>
+            {error && <p className="text-danger">{error}</p>}
+            <Form onSubmit={handleSubmit} id="updateGenre" method="patch">
+                <h2 className="form__title">Modifier le genre</h2>
+                <Form.Group className="mb-3 form__group">
+                    <Form.Label className="form__label" htmlFor="genreLabel">
+                        Genre
+                    </Form.Label>
+                    <Form.Control
+                        className="form__input"
+                        value={formLabel}
+                        onChange={(e) => setFormLabel(e.target.value)}
+                        id="genreLabel"
+                        name="label"
+                        type="text"
+                        placeholder="Entrer le genre"
+                    />
+                </Form.Group>
+                <div className="d-flex form__button__container">
+                    <Button
+                        className="genre__form__button"
+                        type="submit"
+                        disabled={saving}
+                    >
+                        {saving ? "Enregistrement..." : "Enregistrer"}
+                    </Button>
+                </div>
+            </Form>
         </>
-    )
-
+    );
 }
 
 export default UpdateGenreForm;
